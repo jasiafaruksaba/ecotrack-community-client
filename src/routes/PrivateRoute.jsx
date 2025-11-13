ba99e543360b29436e13152b3987e36eda0296de
jasiafaruksaba <sabajasiafaruk@gmail.com>
2025-11-14 02:07:57 +0600

    private routes files

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+// import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router";
+import { useAuth } from "../context/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) return <div className="text-center mt-10">Loading...</div>;
+
+  if (!user) return <Navigate to="/login" state={{ from: location }} replace />;
+
+  return children;
+};
+
+export default PrivateRoute;
