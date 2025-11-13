2799ebc6ea98957df122d19d8b43e91b2b92a061
jasiafaruksaba <sabajasiafaruk@gmail.com>
2025-11-14 02:41:49 +0600

    tipcard files

--- src/components/TipCard.jsx
+++ src/components/TipCard.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
-const TipCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const TipCard = ({ tip }) => (
+  <div className="bg-white shadow rounded p-4 hover:shadow-md transition">
+    <h4 className="font-semibold">{tip.title}</h4>
+    <p className="text-gray-600 mt-1">{tip.content.substring(0, 80)}...</p>
+    <div className="text-gray-500 mt-2 text-sm">By {tip.authorName}</div>
+  </div>
+);
 
 export default TipCard;
