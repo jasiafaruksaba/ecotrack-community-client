514cfd08d7440a26aa6e8184fe72cfe4fe144028
jasiafaruksaba <sabajasiafaruk@gmail.com>
2025-11-14 01:51:57 +0600

    navber files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+// import { Link } from "react-router-dom";
+
+import { Link } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <header className="border-b bg-white">
+      <nav className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3">
+        <Link to="/" className="text-2xl font-bold text-green-600">
+          EcoTrack
+        </Link>
+
+        <div className="flex items-center gap-6">
+          <Link to="/challenges" className="hover:text-green-600">Challenges</Link>
+          <Link to="/my-activities" className="hover:text-green-600">My Activities</Link>
+          <Link to="/login" className="px-4 py-2 border rounded-md hover:bg-green-600 hover:text-white">
+            Login
+          </Link>
         </div>
-    );
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
