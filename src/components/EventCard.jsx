c043cc9334d073706c98761344a92e865057882f
jasiafaruksaba <sabajasiafaruk@gmail.com>
2025-11-14 02:42:40 +0600

    eventcard files

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
-const EventCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const EventCard = ({ event }) => (
+  <div className="bg-white shadow rounded p-4 hover:shadow-md transition">
+    <h4 className="font-semibold">{event.title}</h4>
+    <p className="text-gray-600">{new Date(event.date).toLocaleDateString()} | {event.location}</p>
+    <p className="mt-1 text-gray-700">{event.description.substring(0, 80)}...</p>
+  </div>
+);
 
 export default EventCard;
